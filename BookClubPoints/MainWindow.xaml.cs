4235ccb6b251482e3557e3a00755e9f3ff80303f
krishjan05 <krishjan05@gmail.com>
2016-09-24 11:34:23 -0400

    Added Comments with name of the group members

--- BookClubPoints/MainWindow.xaml.cs
+++ BookClubPoints/MainWindow.xaml.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/* MainWindow.xaml
+   * Created by Krishna Kanhaiya
+   * Assignment 3 
+   * Group Members:
+   * 1. Krishna Kanhaiya
+   * 2. Oleksandr Rudavka
+   * 3. Niral Gandhi
+   * 4. Tanmay Teckchandani
+   * 5. Priya Tharuman
+   * 6. Lucas Currah
+   * 7. Jonathan Nagata
+   * Source: https://github.com/krishjan05/Assignment3Group1
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +38,7 @@ namespace BookClubPoints
         {
             InitializeComponent();
         }
-
+        //Function to calculate the points earned by a member
         private void btnCalculatePoints_Click(object sender, RoutedEventArgs e)
         {
             uint NumberOfBook;
@@ -50,12 +64,12 @@ namespace BookClubPoints
                     MessageBox.Show("Enter Valid Number of books", "Warning");
                 }      
         }
-
+        //Function to reset the value of text box
         private void btnReset_Click(object sender, RoutedEventArgs e)
         {
             txtNumberOfBooks.Text = "0";
         }
-
+        //Function to show the help message box with rules for calculating the points
         private void btnInformation_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("The points are awarded as follows:"+
